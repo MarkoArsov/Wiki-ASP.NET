3e6eac15e9e4b6fe737e38dd114afe68074bc0d3
marrsso <arsovmarko@gmail.com>
2022-05-06 00:14:07 +0200

    Wiki Clone

---
+++ Wiki/Wiki/Models/Entry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wiki.Models
+{
+    public class Entry
+    {   
+
+        public int EntryID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+    }
+}
